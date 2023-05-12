2a54f119beeb0ccb2725accd27639ab5f4204d57
Ricardo Moreno-Ventas <ricardo.morenoventas@softtek.com>
2023-05-12 12:11:21 +0200

    Añadida interfaz de caras
     Para mejor comprensión y trabajo con ello

--- src/com/softtek/modelo/Dado.java
+++ src/com/softtek/modelo/Dado.java
@@ -15,7 +15,37 @@ public class Dado {
         this.minimo = 1;
     }
 
+
+
     public int lanzarDado(){
         return Math.round((float)Math.random() * (maximo - minimo) + minimo);
+    }
+
+    public String vueltaDado(){
+        String salida = "";
+        int sol = this.lanzarDado();
+        switch (sol){
+            case 1:
+                salida = Caras.vacio() + "\n" + Caras.uno() + "\n" + Caras.vacio();
+                break;
+            case 2:
+                salida = Caras.vacio() + "\n" + Caras.dos() + "\n" + Caras.vacio();
+                break;
+            case 3:
+                salida = Caras.vacio() + "\n" + Caras.tres() + "\n" + Caras.vacio();
+                break;
+            case 4:
+                salida = Caras.dos() + "\n" + Caras.vacio() + "\n" + Caras.dos();
+                break;
+            case 5:
+                salida = Caras.dos() + "\n" + Caras.uno() + "\n" + Caras.dos();
+                break;
+            case 6:
+                salida = Caras.tres() + "\n" + Caras.vacio() + "\n" + Caras.tres();
+                break;
+            default:
+                salida = "Error";
+        }
+        return salida;
     }
 }
